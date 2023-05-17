defc2c891e2bc43b6ef60f5ab1dba3edff17690e
eric0326 <a10054026@gmail.com>
2023-05-17 21:07:36 +0800

    5/17 js/99乘法表應用

---
+++ dist/js/js_99.js
@@ -0,0 +1,36 @@
+const genTable = () => {
+    let n1 = document.querySelector('#n1');
+    let n2 = document.querySelector('#n2');
+
+    if (!n1) {
+        return;
+    }
+
+    if (n1.value <= 0) {
+        return;
+    }
+
+    if (!n2) {
+        return;
+    }
+
+    if (n2.value <= 0) {
+        return;
+    }
+
+    let result = [];
+    for (let i = 1; i <= n1.value; i++) {
+        for (let j = 1; j <= n2.value; j++) {
+            result.push(`${i} x ${j} = ${i * j}`);
+        }
+    }
+
+    console.log(result);
+
+
+}
+
+
+let make = document.querySelector('#make');
+
+make.addEventListener('click', genTable);
