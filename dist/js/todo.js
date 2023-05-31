fd4a5a72d4f4045d7bcbc109ccaf90081acfbe51
eric0326 <a10054026@gmail.com>
2023-05-31 20:46:10 +0800

    0531 js/sweetaleart/表單

---
+++ dist/js/todo.js
@@ -0,0 +1,46 @@
+const init = () => {
+    let addBtn = document.querySelector('#add-btn');
+    let itemName = document.querySelector('#item-name');
+
+    // 新增項目統一收納
+    const doAddItem = () => {
+        let value = itemName.value;
+
+        if (!value) {
+            Swal.fire({
+                title: '新增錯誤',
+                text: '請輸入項目名稱',
+                icon: 'error',
+            }).then(() => {
+                setTimeout(() => {
+                    itemName.focus();
+                }, 500)
+            })
+            return;
+        }
+
+        let ul = document.querySelector('#pending-item');
+
+        ul.innerHTML += `
+        <li>
+            <input type="checkbox">
+            <label for="">${value}</label>
+        </li>
+        `;
+    }
+
+    addBtn.addEventListener('click', () => {
+        doAddItem();
+    });
+
+    itemName.addEventListener('keyup', (e) => {
+        if (e.key == 'Enter') {
+
+            doAddItem();
+        }
+    });
+
+
+};
+
+init();
