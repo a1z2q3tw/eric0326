ea3926b76b45f25d3d6e2d1a48435d777a34342c
eric0326 <a10054026@gmail.com>
2023-06-05 21:26:32 +0800

    0605 js 資料庫抓取

--- dist/js/todo_vue.js
+++ dist/js/todo_vue.js
@@ -1,10 +1,68 @@
-Vue.createApp({
+const database = {
+    set(key, value) {
+        value = JSON.stringify(value);
+        localStorage.setItem(key, value);
+    },
+    get(key, def) {
+        let value = localStorage.getItem(key);
+        if (value) {
+            value = JSON.parse(value);
+            return value;
+        }
+        return def;
+    },
+    remove(key) {
+        localStorage.removeItem(key);
+    }
+}
+
+
+let vm = Vue.createApp({
     data() {
         return {
-            name: '888'
+            pending: [],
+            done: [],
+            itemValue: '',
         }
     },
     methods: {
-
+        doAddItem() {
+            let value = this.itemValue;
+            if (!value) {
+                Swal.fire({
+                    title: '新增錯誤',
+                    text: '請輸入項目名稱',
+                    icon: 'error',
+                }).then(() => {
+                    setTimeout(() => {
+                        this.$refs.itemValue.focus();
+                    }, 500)
+                })
+                return;
+            }
+            this.pending.push(value);
+            this.itemValue = '';
+            this.$refs.itemValue.focus();
+            this.update();
+        },
+        toDone(index) {
+            let value = this.pending[index];
+            this.done.push(value);
+            this.pending.splice(index, 1);
+            this.update();
+        },
+        toPending(index) {
+            let value = this.done[index];
+            this.pending.push(value);
+            this.done.splice(index, 1);
+            this.update();
+        },
+        update() {
+            database.set('todo-pending', this.pending);
+            database.set('todo-done', this.done);
+        }
+    },
+    mounted() {
+        this.peding = database.get('todo-pending', []);
     }
 }).mount('#app');
