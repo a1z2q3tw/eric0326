c82b7b0a0d9a40da5adefea49b216d81efcd4146
eric0326 <a10054026@gmail.com>
2023-06-12 21:29:50 +0800

    6/12 jQuery

--- dist/js/todo_vue.js
+++ dist/js/todo_vue.js
@@ -60,7 +60,89 @@ let vm = Vue.createApp({
         update() {
             database.set('todo-pending', this.pending);
             database.set('todo-done', this.done);
+        },
+        doSaveCloud() {
+            Swal.fire({
+                title: '儲存到雲端',
+                text: '請輸入 UID',
+                input: 'text',
+                showCancelButton: true,
+                confirmButtonText: '儲存',
+                cancelButtonText: '先不要'
+            }).then(response => {
+                if (response.value) {
+                    let api = 'https://book.niceinfos.com/frontend/api/'
+
+                    // 使用表單物件
+                    // let form = new FormData;
+                    // form.append('action', 'todo');
+                    // form.append('uid', response.value);
+                    // form.append('data', {
+                    //     pending: this.pending,
+                    //     done: this.done
+                    // });
+
+                    let params = {
+                        action: 'todo',
+                        uid: response.value,
+                        data: {
+                            pending: this.pending,
+                            done: this.done
+                        }
+                    }
+
+                    fetch(api, {
+                        method: 'POST',
+                        body: JSON.stringify(params)
+                    }).then(response => {
+                        return response.text();
+                    }).then(text => {
+                        setTimeout(() => {
+                            let data = JSON.parse(text);
+                            console.log(data);
+                            Swal.fire({
+                                title: '儲存完畢',
+                                text: '資料已儲存',
+                                icon: 'success'
+                            })
+                        }, 1000);
+                    })
+                    Swal.fire({
+                        title: '資料儲存中',
+                        text: '請勿關閉或重新整理視窗',
+                        showConfirmButton: false
+
+                    })
+                }
+            });
+        },
+        async doLoadCloud() {
+            let response = await Swal.fire({
+                title: '載入雲端資料',
+                text: '請輸入 UID',
+                input: 'text'
+            });
+
+            if (response.value) {
+                let api = 'https://book.niceinfos.com/frontend/api/';
+                fetch(`${api}?action=todo&uid=${response.value}`).then(response => {
+                    return response.text();
+                }).then(text => {
+
+                    let response = JSON.parse(text);
+                    if (response.data.pending) {
+                        this.pending = response.data.pending;
+                    }
+
+                    if (response.data.done) {
+                        this.done = response.data.done;
+                    }
+
+                    this.update();
+                })
+            }
         }
+
     },
     mounted() {
         this.peding = database.get('todo-pending', []);
